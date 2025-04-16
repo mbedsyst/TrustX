1ea7e8dc2a4b376b68448ceb3c3a0821b3027344
Zalman Ul Fariz P A <zalman15320011351@gmail.com>
2025-04-17 03:03:13 +0530

    Commented Parser source & header file

--- Application/Protocol/PacketParser/Inc/PacketParser.h
+++ Application/Protocol/PacketParser/Inc/PacketParser.h
@@ -4,9 +4,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Upper limit on valid INPUT_DATA in Data Stream
 #define MAX_INPUT_DATA_SIZE 	4096
-#define EOD_FLAG_VALUE 			0xDEADBEEF
 
+// Structure to hold incoming Data Stream
 typedef struct
 {
     uint32_t transactionID;
@@ -16,6 +17,7 @@ typedef struct
     uint8_t inputData[MAX_INPUT_DATA_SIZE];
 } ParsedPacket_t;
 
+// Enumeration to hold Parse Operation status
 typedef enum
 {
     PARSE_SUCCESS = 0,
@@ -25,7 +27,7 @@ typedef enum
     PARSE_INPUT_TOO_LARGE
 } ParseStatus_t;
 
+// Function to Parse contents from Data Stream to fill Packet Structure
 ParseStatus_t PacketParser_Parse(const uint8_t* rawData, uint16_t rawLength, ParsedPacket_t* parsedPacket);
-
 
 #endif /* PROTOCOL_PACKETPARSER_INC_PACKETPARSER_H_ */
--- Application/Protocol/PacketParser/Src/PacketParser.c
+++ Application/Protocol/PacketParser/Src/PacketParser.c
@@ -1,43 +1,56 @@
 #include "../../PacketParser/Inc/PacketParser.h"
+#include "constants.h"
 #include <string.h>
 
 ParseStatus_t PacketParser_Parse(const uint8_t* rawData, uint16_t rawLength, ParsedPacket_t* parsedPacket)
 {
-    if (!rawData || !parsedPacket) return PARSE_NULL_POINTER;
-    if (rawLength < 12) return PARSE_INVALID_LENGTH; // Minimum packet size
-
-    // Extract fields
+	// Check if Data Stream or Packet structure is NULL
+    if (!rawData || !parsedPacket)
+    {
+    	return PARSE_NULL_POINTER;
+    }
+    // Check if Data Stream contains Data of valid length
+    if (rawLength < 12)
+    {
+    	return PARSE_INVALID_LENGTH;
+    }
+    // Set Index to start of Data Stream
     uint16_t index = 0;
-
+    // Retrieve Transaction ID from Data Stream
     parsedPacket->transactionID = (rawData[index]     << 24) |
                                   (rawData[index + 1] << 16) |
                                   (rawData[index + 2] << 8)  |
                                   (rawData[index + 3]);
+    // Set Index to point CMD byte
     index += 4;
-
+    // Retrieve CMD byte from Data Stream
     parsedPacket->cmd = rawData[index++];
+    // Retrieve OPTION byte from Data Stream
     parsedPacket->option = rawData[index++];
-
+    // Retrieve INPUT_SIZE half-word from Data Stream
     parsedPacket->inputSize = (rawData[index] << 8) | rawData[index + 1];
+    // Set Index to point INPUT_DATA
     index += 2;
-
+    // Check if INPUT_SIZE is under the prescribed limit
     if (parsedPacket->inputSize > MAX_INPUT_DATA_SIZE || (index + parsedPacket->inputSize + 4) > rawLength)
     {
         return PARSE_INPUT_TOO_LARGE;
     }
-
+    // Copy INPUT_DATA array from Data Stream to Packet structure
     memcpy(parsedPacket->inputData, &rawData[index], parsedPacket->inputSize);
+    // Set Index to point EOD Flag
     index += parsedPacket->inputSize;
-
+    // Retrieve EOD Flag from Data Stream
     uint32_t receivedEOD = (rawData[index]     << 24) |
                            (rawData[index + 1] << 16) |
                            (rawData[index + 2] << 8)  |
                            (rawData[index + 3]);
-
-    if (receivedEOD != EOD_FLAG_VALUE) {
+    // Check if EOD Flag is valid
+    if (receivedEOD != EOD_FLAG_VALUE)
+    {
         return PARSE_EOD_MISMATCH;
     }
-
+    // Return to Indicate successful parsing
     return PARSE_SUCCESS;
 }
 
