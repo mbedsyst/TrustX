eebeb7a7066d6ec51dd954afdfdf5ed5bac6f9de
Zalman Ul Fariz P A <zalman15320011351@gmail.com>
2025-04-17 03:15:52 +0530

    Commented Packet Builder source & header files

--- Application/Protocol/PacketBuilder/Inc/PacketBuilder.h
+++ Application/Protocol/PacketBuilder/Inc/PacketBuilder.h
@@ -4,9 +4,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Upper limit on valid OUTPUT_DATA in Data Stream
 #define MAX_OUTPUT_DATA_SIZE 4096
-#define EOD_FLAG_VALUE 0xDEADBEEF
 
+// Structure to hold outgoing Data Stream
 typedef struct
 {
     uint32_t transactionID;
@@ -14,6 +15,7 @@ typedef struct
     uint8_t outputData[MAX_OUTPUT_DATA_SIZE];
 } ResponsePacket_t;
 
+// Enumeration to hold Build Operation status
 typedef enum
 {
     BUILD_SUCCESS = 0,
@@ -21,8 +23,7 @@ typedef enum
     BUILD_OUTPUT_TOO_LARGE
 } BuildStatus_t;
 
-// Builds a byte stream from a structured response packet
+// Function to Build Data Stream from Packet contents
 BuildStatus_t PacketBuilder_Build(const ResponsePacket_t* responsePacket, uint8_t* outBuffer, uint16_t* outLength);
-
 
 #endif /* PROTOCOL_PACKETBUILDER_INC_PACKETBUILDER_H_ */
--- Application/Protocol/PacketBuilder/Src/PacketBuilder.c
+++ Application/Protocol/PacketBuilder/Src/PacketBuilder.c
@@ -1,34 +1,41 @@
 #include "../../PacketBuilder/Inc/PacketBuilder.h"
+#include "constants.h"
 #include <string.h>
+
 
 BuildStatus_t PacketBuilder_Build(const ResponsePacket_t* responsePacket, uint8_t* outBuffer, uint16_t* outLength)
 {
-    if (!responsePacket || !outBuffer || !outLength) return BUILD_NULL_POINTER;
-    if (responsePacket->outputSize > MAX_OUTPUT_DATA_SIZE) return BUILD_OUTPUT_TOO_LARGE;
-
+	// Check if Response, Output and Output Size are NULL
+    if (!responsePacket || !outBuffer || !outLength)
+    {
+    	return BUILD_NULL_POINTER;
+    }
+    // Check if Output Size is under the specified limits
+    if (responsePacket->outputSize > MAX_OUTPUT_DATA_SIZE)
+    {
+    	return BUILD_OUTPUT_TOO_LARGE;
+    }
+    // Set Index to start of Data Stream
     uint16_t index = 0;
-
-    // Transaction ID
+    // Fill Data Stream with the corresponding Transaction ID
     outBuffer[index++] = (responsePacket->transactionID >> 24) & 0xFF;
     outBuffer[index++] = (responsePacket->transactionID >> 16) & 0xFF;
     outBuffer[index++] = (responsePacket->transactionID >> 8)  & 0xFF;
     outBuffer[index++] = (responsePacket->transactionID)       & 0xFF;
-
-    // Output Size
+    // Fill Data Stream with the Output Data Size
     outBuffer[index++] = (responsePacket->outputSize >> 8) & 0xFF;
     outBuffer[index++] = (responsePacket->outputSize)      & 0xFF;
-
-    // Output Data
+    // Copy Output Data contents to Data Stream
     memcpy(&outBuffer[index], responsePacket->outputData, responsePacket->outputSize);
+    // Set Index to point to EOD Flag
     index += responsePacket->outputSize;
-
-    // EOD Flag
+    // Add EOD Flag to Data Stream
     outBuffer[index++] = (EOD_FLAG_VALUE >> 24) & 0xFF;
     outBuffer[index++] = (EOD_FLAG_VALUE >> 16) & 0xFF;
     outBuffer[index++] = (EOD_FLAG_VALUE >> 8)  & 0xFF;
     outBuffer[index++] = (EOD_FLAG_VALUE)       & 0xFF;
-
+    // Set Index to point to End of Data Stream
     *outLength = index;
-
+    // Return to indicate successful Packet Build
     return BUILD_SUCCESS;
 }
