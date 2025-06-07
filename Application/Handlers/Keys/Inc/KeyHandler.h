164a16c65f35e952aa369635317d8f8192951964
Zalman Ul Fariz P A <zalman15320011351@gmail.com>
2025-06-07 23:24:54 +0530

    Ported Random Handler for Key Generation operation

--- Application/Handlers/Keys/Inc/KeyHandler.h
+++ Application/Handlers/Keys/Inc/KeyHandler.h
@@ -1,13 +1,10 @@
-/*
- * KeyHandler.h
- *
- *  Created on: Apr 17, 2025
- *      Author: zalma
- */
-
 #ifndef HANDLERS_KEYS_INC_KEYHANDLER_H_
 #define HANDLERS_KEYS_INC_KEYHANDLER_H_
 
+#include "types.h"
+#include "PacketParser.h"
+#include "PacketBuilder.h"
 
+OperationStatus_t KeyHandler_Handle(const ParsedPacket_t* request, ResponsePacket_t* response);
 
 #endif /* HANDLERS_KEYS_INC_KEYHANDLER_H_ */
