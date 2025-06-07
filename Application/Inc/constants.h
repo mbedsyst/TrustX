a3aff606463644f104a0bb24b7491eacd797f79e
Zalman Ul Fariz P A <zalman15320011351@gmail.com>
2025-06-07 23:29:05 +0530

    Defined constants for featured OTP & Key sizes

--- Application/Inc/constants.h
+++ Application/Inc/constants.h
@@ -6,9 +6,10 @@
 #define CMD_DECRYPT               0x02 // 0b00000010
 #define CMD_HASH                  0x03 // 0b00000011
 #define CMD_RANDOM                0x04 // 0b00000100
-#define CMD_KEY_STORE             0x05 // 0b00000101
-#define CMD_KEY_RETRIEVE          0x06 // 0b00000110
-#define CMD_KEY_ERASE             0x07 // 0b00000111
+#define CMD_OTP					  0x05 // 0b00000101
+#define CMD_KEY_GEN               0x06 // 0b00000101
+#define CMD_KEY_RETRIEVE          0x07 // 0b00000110
+#define CMD_KEY_ERASE             0x08 // 0b00000111
 #define CMD_PING                  0x0F // 0b00001111    For testing HSM connection
 
 // === Option Codes (OPTION field) ===
@@ -29,10 +30,14 @@
 #define OPTION_RNG_0004           0x31 // 0b00110001
 #define OPTION_RNG_0008           0x32 // 0b00110010
 #define OPTION_RNG_0016           0x33 // 0b00110011
+#define OPTION_RNG_0024           0x3C // 0b00111100
 #define OPTION_RNG_0032           0x34 // 0b00110100
+#define OPTION_RNG_0048           0x3D // 0b00111101
 #define OPTION_RNG_0064           0x35 // 0b00110101
+#define OPTION_RNG_0066			  0x3E // 0b00111110
 #define OPTION_RNG_0128           0x36 // 0b00110110
 #define OPTION_RNG_0256           0x37 // 0b00110111
+#define OPTION_RNG_0384			  0x3F // 0b00111111
 #define OPTION_RNG_0512           0x38 // 0b00111000
 #define OPTION_RNG_1024           0x39 // 0b00111001
 #define OPTION_RNG_2048           0x3A // 0b00111010
