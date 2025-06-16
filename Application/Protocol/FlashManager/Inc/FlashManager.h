945279bdb636acf9203b900d5a55b105e4980fc1
Zalman Ul Fariz P A <zalman15320011351@gmail.com>
2025-06-16 23:54:08 +0530

    Updating Flash Manager functions

--- Application/Protocol/FlashManager/Inc/FlashManager.h
+++ Application/Protocol/FlashManager/Inc/FlashManager.h
@@ -1,6 +1,27 @@
 #ifndef PROTOCOL_FLASHMANAGER_INC_FLASHMANAGER_H_
 #define PROTOCOL_FLASHMANAGER_INC_FLASHMANAGER_H_
 
+#include "stdint.h"
 
+typedef enum
+{
+    FLASH_MANAGER_OK = 0,             // Operation successful
+    FLASH_MANAGER_ERROR = 1,          // Generic error
+    FLASH_MANAGER_WRITE_FAIL = 2,     // Failed to write to flash
+    FLASH_MANAGER_READ_FAIL = 3,      // Failed to read from flash
+    FLASH_MANAGER_ERASE_FAIL = 4,     // Failed to erase flash
+    FLASH_MANAGER_INVALID_FLAG = 5,   // Invalid Flag Value
+    FLASH_MANAGER_BUSY = 6,           // Flash is currently busy
+    FLASH_MANAGER_TIMEOUT = 7,        // Operation timed out
+    FLASH_MANAGER_NOT_INITIALIZED = 8 // FlashManager not initialized
+} FlashManagerStatus_t;
+
+FlashManagerStatus_t FlashManager_Init(void);
+FlashManagerStatus_t FlashManager_ReadIdentifier(uint16_t sectorNumber, uint8_t *entryHeader);
+FlashManagerStatus_t FlashManager_ReadEntry(uint16_t sectorNumber, uint8_t *entry);
+FlashManagerStatus_t FlashManager_WriteEntry(uint16_t sectorNumber, uint8_t *entry);
+FlashManagerStatus_t FlashManager_UpdateEntry(uint16_t sectorNumber, uint8_t *entry);
+FlashManagerStatus_t FlashManager_InvalidateEntry(uint16_t sectorNumber);
+FlashManagerStatus_t FlashManager_EraseEntry(uint16_t sectorNumber);
 
 #endif /* PROTOCOL_FLASHMANAGER_INC_FLASHMANAGER_H_ */
--- Application/Protocol/FlashManager/Src/FlashManager.c
+++ Application/Protocol/FlashManager/Src/FlashManager.c
@@ -9,15 +9,29 @@
 #define ENTRY_SIZE		88
 #define VALID_FLAG 		0xAA
 #define INVALID_FLAG 	0x55
+#define IDENTIFIER_SIZE	5
 
-void FlashManager_Init(void)
+FlashManagerStatus_t FlashManager_Init(void)
 {
+	uint32_t deviceID = 0;
 	log_info("Initializing the Flash Manager.");
 	W25Q_Reset();
+	deviceID = W25Q_ReadID();
 	log_info("External Flash Memory device has been reset.");
+	log_info("Flash Memory Device ID : %0xX", deviceID);
+	return FLASH_MANAGER_OK;
 }
 
-void FlashManager_ReadEntry(uint16_t sectorNumber, uint8_t *entry)
+FlashManagerStatus_t FlashManager_ReadIdentifier(uint16_t sectorNumber, uint8_t *entryHeader)
+{
+	// Calculating Page Number from Sector Number
+	uint32_t page_number = sectorNumber * (SECTOR_SIZE/PAGE_SIZE);
+	W25Q_Read(page_number, 0, IDENTIFIER_SIZE, entryHeader);
+	log_info("Read Sector Identifier from Flash.");
+	return FLASH_MANAGER_OK;
+}
+
+FlashManagerStatus_t FlashManager_ReadEntry(uint16_t sectorNumber, uint8_t *entry)
 {
 	// Calculating Page Number and Validity Flag address.
 	uint32_t page_number = sectorNumber * (SECTOR_SIZE/PAGE_SIZE);
@@ -29,16 +43,17 @@ void FlashManager_ReadEntry(uint16_t sectorNumber, uint8_t *entry)
 	{
 		// ToDo Fix Error Handling
 		log_error("Entry has Invalid Flag.");
+		return FLASH_MANAGER_INVALID_FLAG;
 	}
 	else if(entryFlag == VALID_FLAG)
 	{
 		// Reading from the sector if validity flag is 0xAA
-		W25Q_Read(page_number, 1, KEY_ENTRY_SIZE, entry);
+		W25Q_Read(page_number, 1, ENTRY_SIZE, entry);
 		log_info("Read valid entry from Flash.");
 	}
 }
 
-void FlashManager_WriteEntry(uint16_t sectorNumber, uint8_t *entry)
+FlashManagerStatus_t FlashManager_WriteEntry(uint16_t sectorNumber, uint8_t *entry)
 {
 	// Calculating Page Number and Validity Flag address
 	uint32_t page_number = sectorNumber * (SECTOR_SIZE/PAGE_SIZE);
@@ -51,6 +66,7 @@ void FlashManager_WriteEntry(uint16_t sectorNumber, uint8_t *entry)
 		// ToDo Fix Error Handling
 		log_error("Entry has a Valid Flag.");
 		log_warn("Cannot write to this sector.");
+		return FLASH_MANAGER_ERROR;
 	}
 	else
 	{
@@ -61,7 +77,7 @@ void FlashManager_WriteEntry(uint16_t sectorNumber, uint8_t *entry)
 	}
 }
 
-void FlashManager_UpdateEntry(uint16_t sectorNumber, uint8_t *entry)
+FlashManagerStatus_t FlashManager_UpdateEntry(uint16_t sectorNumber, uint8_t *entry)
 {
 	// Calculating Page Number and Validity Flag address
 	uint32_t page_number = sectorNumber * (SECTOR_SIZE/PAGE_SIZE);
@@ -71,8 +87,8 @@ void FlashManager_UpdateEntry(uint16_t sectorNumber, uint8_t *entry)
 	// Checking if Entry has an Invalid Flag or not.
 	if (entryFlag == INVALID_FLAG)
 	{
-		// ToDo Fix Error Handling
 		log_error("Entry has an Invalid Flag.");
+		return FLASH_MANAGER_ERROR;
 	}
 	else if(entryFlag == VALID_FLAG)
 	{
@@ -86,7 +102,29 @@ void FlashManager_UpdateEntry(uint16_t sectorNumber, uint8_t *entry)
 	}
 }
 
-void FlashManager_EraseEntry(uint16_t sectorNumber, uint8_t *entry)
+FlashManagerStatus_t FlashManager_InvalidateEntry(uint16_t sectorNumber)
+{
+	// Calculating Page Number and Validity Flag address
+	uint32_t page_number = sectorNumber * (SECTOR_SIZE/PAGE_SIZE);
+	uint32_t byte_addr = page_number * PAGE_SIZE;
+	// Writing Invalid Flag to start of Header
+	W25Q_Write_Byte(byte_addr, INVALID_FLAG);
+	// Reading Validity Flag at the start of sector
+	uint8_t entryFlag = W25Q_Read_Byte(byte_addr);
+	// Checking if Entry has a Valid Flag or not.
+	if (entryFlag == VALID_FLAG)
+	{
+		log_info("The current entry has a Valid Flag.");
+		return FLASH_MANAGER_WRITE_FAIL;
+	}
+	else
+	{
+		log_info("The entry has been Invalidated.");
+		return FLASH_MANAGER_OK;
+	}
+}
+
+FlashManagerStatus_t FlashManager_EraseEntry(uint16_t sectorNumber)
 {
 	// Calculating Page Number and Validity Flag address
 	uint32_t page_number = sectorNumber * (SECTOR_SIZE/PAGE_SIZE);
@@ -102,8 +140,8 @@ void FlashManager_EraseEntry(uint16_t sectorNumber, uint8_t *entry)
 	}
 	else
 	{
-		// ToDo Fix Error Handling
 		log_info("The current entry has an Invalid Flag.");
+		return FLASH_MANAGER_ERROR;
 	}
 }
 
